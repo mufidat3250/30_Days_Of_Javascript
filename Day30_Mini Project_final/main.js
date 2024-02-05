f7105d55f90fbc4080b834fd0b59eb8d7462b1cf
Wahab MMufidat <wahabmufidat1919@gmail.com>
2024-02-04 22:45:08 -0500

    started day 30 logic

--- Day30_Mini Project_final/main.js
+++ Day30_Mini Project_final/main.js
@@ -1,52 +1,56 @@
-import{countries} from './countries.js'
+import{countries, oldCountries} from './countries/countries.js'
 const body = document.querySelector('body')
 const input = document.querySelector('input')
 const btn = document.querySelectorAll('button')
 const countryContainer = document.querySelector('.country-container')
 
-btn.forEach((btn)=> {
-    btn.addEventListener('click', ()=>{
-        let inputValue = input.value.trim()
-        if(btn.name === 'name'){
-            getAllCounntries(inputValue, btn.name)
-        }else if(btn.name === 'population'){
-            console.log('population', input.value)
-        }else if(btn.name === 'capital'){
-            console.log('capital', inputValue)
-        }else if(btn.name === 'chart-icon'){
-            console.log('chartIcon', inputValue)
-        }
-    })
-})
+let collectecCountries = [...countries]
+let inputValue = input.value.trim()
+input.addEventListener('input', (e)=> {
+  countryContainer.innerHTML = ''
+    let inputValue = e.target.value.trim()
+    let filtedArray = collectecCountries.filter((country)=> country.name.toLowerCase().includes(inputValue.toLowerCase()))
+    getAllCounntries(filtedArray)
+}, false)
 
-console.log(btn[0], btn[1], btn[2])
+//name
+let counter = 0
 btn[0].addEventListener('click', ()=> {
+  let filtedArray = collectecCountries.filter((country)=> country.name.toLowerCase().includes(inputValue.toLowerCase()))
+  console.log({filtedArray})
+  counter ++
 
+  if(counter % 2 !== 0 ){
+    countryContainer.innerHTML = ''
+    let acendingSort = filtedArray.sort((a, b)=> b.name > a.name ? 1 : b.name < a.name ? -1 : 0  )
+    getAllCounntries(acendingSort)
+  }else if (counter % 2 === 0){
+    countryContainer.innerHTML = ''
+    let decendingSort = filtedArray.sort((a, b)=> a.name > b.name ? 1 : a.name < b.name ? -1 : 0  )
+    console.log('decending', decendingSort)
+    getAllCounntries(decendingSort)
+  }
 })
 
-const getAllCounntries = (name = 'all', inputValue) => {
-    console.log(inputValue)
-    if(!inputValue && name === 'all'){
-        countries.map((country)=> {
-            let {name, capital, flag, population, languages } = country
-            const countryCard = document.createElement('div')
-            countryCard.setAttribute('class', 'country-card') 
-            countryContainer.appendChild(countryCard)
-            countryCard.innerHTML = `
-            <img
-            src='${flag}'
-            alt=""
-            class="flag"
-          />
-            <h4>${name}</h4>
-            <p>Capital: ${capital} </p>
-            <p style = ''>Language: <span>${languages.join(',\n')}</span></p>
-            <p>Population:${population}</p>
-          `
-        })
-    }else if(inputValue && name === 'name'){
-    
-    }
+
+const getAllCounntries = (data = collectecCountries) => {
+    data.map((country)=> {
+        let {name, capital, flag, population, languages } = country
+        const countryCard = document.createElement('div')
+        countryCard.setAttribute('class', 'country-card') 
+        countryContainer.appendChild(countryCard)
+        countryCard.innerHTML = `
+        <img
+        src="${flag}"
+        alt=""
+        class="flag"
+      />
+        <h4>${name}</h4>
+        <p>Capital: ${capital} </p>
+        <p style = ''>Language: <span>${languages.join(',\n')}</span></p>
+        <p>Population:${population}</p>
+      `
+    })
 }
 
 
